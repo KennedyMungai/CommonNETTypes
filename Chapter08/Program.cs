9d06ea1d3ea0e4a1fc793dfb8e1cdcc7cbaf9835
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-05-27 08:41:35 +0300

    Numbers stuff

--- Chapter08/Program.cs
+++ Chapter08/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace CommonNETTypes.Chapter08;
+﻿using System.Numerics;
+
+using static System.Console;
+
+namespace CommonNETTypes.Chapter08;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        
+        WriteLine("Working with large integers");
+        WriteLine("----------------------------");
+
+        ulong big = ulong.MaxValue;
+        WriteLine($"{big,40:N0}");
+
+        BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+
+        WriteLine($"{bigger,40:N0}");
     }
 }
